35978aac9fc0e1a2a7fa1170b437f20e7ec009f5
frsargua <frsargua@gmail.com>
2023-06-01 21:06:09 +0100

    Add verification for radius, lat, lon and isCircle parameters in GetSpots handler function.

--- controllers/spot.go
+++ controllers/spot.go
@@ -2,7 +2,10 @@ package controllers
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"regexp"
+	"strconv"
 
 	"github.com/frsargua/GoLangTest/models"
 )
@@ -10,9 +13,57 @@ import (
 
 func GetSpots(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application/json")
+	queryParams := r.URL.Query()
+	latitude := queryParams.Get("latitude")
+	longitude := queryParams.Get("longitude") 
+	radiusStr := queryParams.Get("radius") 
+	isCircleStr := queryParams.Get("isCircle") 
+
+
+		// Check if any parameter is missing
+		if latitude == "" || longitude == "" || radiusStr == "" || isCircleStr == "" {
+			err := errors.New("One or more parameters are missing")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+			// Verify latitude and longitude format
+	if !isValidCoordinate(latitude) || !isValidCoordinate(longitude) {
+		err := errors.New("Invalid latitude or longitude")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Verify radius is an integer
+	radius, err := strconv.Atoi(radiusStr)
+	if err != nil {
+		err := errors.New("Invalid radius")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// Verify isCircle is a boolean
+	isCircle, err := strconv.ParseBool(isCircleStr)
+	if err != nil {
+		err := errors.New("Invalid isCircle")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var results []models.Spot
+	
+	results = getSpotsInArea(latitude,longitude,radius,isCircle);
   
-	var spots []models.Spot
-	models.DB.Find(&spots)
-  
-	json.NewEncoder(w).Encode(spots)
+	json.NewEncoder(w).Encode(results)
   }
+
+  func isValidCoordinate(coordinate string) bool {
+	regex := `^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?)$`
+	match, _ := regexp.MatchString(regex, coordinate)
+	return match
+}
+
+func getSpotsInArea(latitude string, longitude string, radiusStr int, isCircleStr bool) []models.Spot {
+	var spots []models.Spot
+	models.DB.Limit(10).Find(&spots)
+	return spots
+}
